d8215f11bbf2f4783762568a304ef621fac1a2c1
Hannah Zabarsky <hannah@eyenetra.com>
2015-03-23 13:27:13 -0400

    removed lib and beginning of bracket requests

--- lib/request.rb
+++
@@ -1,7 +0,0 @@
-http.get SCOREBOARD, :new_wins
-
-SCOREBOARD = "http://data.ncaa.com/jsonp/scoreboard/basketball-men/d1/2015/03/19/scoreboard.html?callback=ncaaScoreboard.dispScoreboard"
-
-def new_wins(request, response)
-  response.body
-end
